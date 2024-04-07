c73d8122e8bfc60fddfc022771bef3a40ad8277c
XxThunderBlastxX <koustavmondal55@gmail.com>
2024-04-07 21:08:38 +0530

    Added redirection on login

--- lib/src/app/router/router.dart
+++ lib/src/app/router/router.dart
@@ -5,9 +5,13 @@ import '../../feature/auth/presentation/screens/login_screen.dart';
 import '../../feature/auth/presentation/screens/signup_screen.dart';
 import '../../feature/chat/presentation/screens/chat_home_screen.dart';
 import '../../feature/chat/presentation/screens/chat_message_screen.dart';
+import '../service/supabase_service.dart';
+import '../service_locator/service_locator.dart';
 
 GoRouter router = GoRouter(
-  initialLocation: "/login",
+  initialLocation: sl<SupabaseService>().client.auth.currentSession != null
+      ? "/home"
+      : "/login",
   routes: [
     GoRoute(
       path: "/login",
